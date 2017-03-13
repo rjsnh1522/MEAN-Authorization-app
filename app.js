f02170adf1d9b29f5d76f5ca3a5f3e2e3cddc173
logan <rjsnh1522@gmail.com>
2017-03-14 02:26:12 +0530

    heroku commit

---
+++ app.js
@@ -0,0 +1,74 @@
+const express=require('express');
+const path=require('path');
+const bodyParser=require('body-parser');
+const cors =require('cors');
+// const passport=require('passport-jwt');
+var passport = require('passport');
+const mongoose =require('mongoose');
+const config=require('./config/database.js');
+
+
+mongoose.connect(config.database);
+// //on connecton
+mongoose.connection.on('connected',()=>{
+  console.log("connected to database"+config.database);
+});
+
+//on error
+mongoose.connection.on('error',(err)=>{
+  console.log("database err"+err);
+});
+
+
+const app=express();
+
+const users =require('./routes/users');
+
+// const port =3000; this is for local
+
+// for heroku
+const port =process.env.PORT || 8080;
+
+
+
+
+app.use(cors());
+
+//set static folder
+app.use(express.static(path.join(__dirname,'public')));
+
+
+
+//body paser Middleware
+
+app.use(bodyParser.json());
+
+//Passport Middleware
+
+app.use(passport.initialize());
+
+app.use(passport.session());
+
+require('./config/passport')(passport);
+
+app.use('/users',users);
+
+//index route
+app.get('/',(req,res)=>{
+
+  res.send("Invalid Endpoint");
+});
+
+
+app.get('*',(req,res)=>{
+
+  res.sendFile(path.join(__dirname,'public/index.html'));
+});
+
+//start server
+//using arrow function
+app.listen(port,()=>{
+
+  console.log("listening to port "+port);
+
+});
---
+++ routes/users.js
@@ -0,0 +1,95 @@
+const express=require('express');
+const router=express.Router();
+const passport=require('passport');
+const jwt=require('jsonwebtoken');
+const User=require('../models/user');
+const config=require('../config/database')
+//register
+router.post('/register',(req,res,next)=>{
+
+    let newUser= new User({
+        name:req.body.name,
+        email:req.body.email,
+        username:req.body.username,
+        password:req.body.password
+
+    });
+
+    User.addUser(newUser,(err,user)=>{
+            if(err){
+               res.json({success:false,msg:'failed to register'});
+            }
+            else{
+                res.json({success:true,msg:'user registered'});
+
+            }
+
+    });
+
+    // res.send('Register');
+});
+
+//Authenticate
+
+router.post('/authenticate',(req,res,next)=>{
+    const username=req.body.username;
+    const password=req.body.password;
+
+    User.getUserByUsername(username,(err,user)=>{
+
+        if(err) throw err;
+
+        if(!user){
+            return res.json({success:false,msg:'user not found'});
+        }
+
+        User.comparePassword(password,user.password,(err,isMatch)=>{
+
+            if(err) throw err;
+            if(isMatch){
+                const token =jwt.sign(user,config.secret,{
+                    expiresIn:604800
+                });
+
+                res.json({
+                    success:true,
+                    token:'JWT '+token,
+                    user:{
+                        id:user._id,
+                        name:user.username,
+                        email:user.email
+                    }
+                });
+            }else{
+
+                return res.json({success:false,msg:"Wrong Password"});
+            }
+
+        });
+
+    });
+
+
+
+
+
+
+
+
+
+});
+//profile
+router.get('/profile',passport.authenticate('jwt',{session:false}), (req,res,next)=>{
+
+    console.log(req.user);
+    res.json({user:req.user});
+    // res.send('profile');
+});
+
+//validate
+router.get('/validate',(req,res,next)=>{
+    res.send('validate');
+});
+
+
+module.exports=router;
